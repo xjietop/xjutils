f0d533317d207e5f161515074bcea8a1e6ea0cc9
老马 <107077584@qq.com>
2020-07-17 21:58:12 +0800

    v0.1.1 2020-07-17 增加RpcxCallStr,RpcxCallByte函数调用rpcx

--- rpcx.go
+++ rpcx.go
@@ -21,3 +21,33 @@ func RpcxCall(etcdAddr []string, servicePath, method string, args interface{}) (
 	}
 	return
 }
+
+func RpcxCallStr(etcdAddr []string, servicePath, method string, args interface{}) (reply string, err error){
+	basePath := ""
+	index := strings.LastIndex(servicePath,"/")
+	service := servicePath[index+1:]
+	d := client.NewEtcdDiscovery(basePath, servicePath,etcdAddr, nil)
+	xclient := client.NewXClient(service, client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer xclient.Close()
+
+	err = xclient.Call(context.Background(), method, args, &reply)
+	if err != nil {
+		log.Printf("failed to call: %v", err)
+	}
+	return
+}
+
+func RpcxCallByte(etcdAddr []string, servicePath, method string, args interface{}) (reply []byte, err error){
+	basePath := ""
+	index := strings.LastIndex(servicePath,"/")
+	service := servicePath[index+1:]
+	d := client.NewEtcdDiscovery(basePath, servicePath,etcdAddr, nil)
+	xclient := client.NewXClient(service, client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer xclient.Close()
+
+	err = xclient.Call(context.Background(), method, args, &reply)
+	if err != nil {
+		log.Printf("failed to call: %v", err)
+	}
+	return
+}
